e872732b3f2c4c759d3e98e364eab44c48dea190
Thitiphatx <thitiphat6391@gmail.com>
2024-11-09 13:33:43 +0700

    move submodule

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
         domains: ['www.prydwen.gg']
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/images/:path*',
+        destination: '/src/resonance.cdn/cdn/characters/icons/:path*', // Points to your image directory
+      },
+    ];
+  },
 };
 
 export default nextConfig;
